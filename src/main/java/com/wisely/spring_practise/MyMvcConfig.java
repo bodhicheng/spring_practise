1827b158c4a9c9583305c437128896f152a39ffd
bodhicheng <nwct@qq.com>
2019-06-27 18:06:44 +0800

    '4.3.1~4.4.3'

--- src/main/java/com/wisely/spring_practise/MyMvcConfig.java
+++ src/main/java/com/wisely/spring_practise/MyMvcConfig.java
@@ -1,16 +1,17 @@
 package com.wisely.spring_practise;
 
+import com.wisely.spring_practise.interceptor.DemoInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
-@EnableWebMvc
+@EnableWebMvc //1
 @ComponentScan("com.wisely.spring_practise")
-public class MyMvcConfig {
+public class MyMvcConfig extends WebMvcConfigurerAdapter { //2
     @Bean
     public InternalResourceViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -18,5 +19,21 @@ public class MyMvcConfig {
         viewResolver.setSuffix(".jsp");
         viewResolver.setViewClass(JstlView.class);
         return viewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/assets/**")
+                .addResourceLocations("classpath:/assets/"); //3
+    }
+
+    @Bean
+    public DemoInterceptor demoInterceptor() {
+        return new DemoInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(demoInterceptor());
     }
 }
